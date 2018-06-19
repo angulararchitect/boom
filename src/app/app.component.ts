55c24967bd2e10f2de346e03a608f2d966b6e04a
amit <web.engineer.amitsinghgmail.com>
2018-06-19 21:57:07 +0530

    Added Sapna

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component,OnInit,ViewContainerRef } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  subject= 'Doller Ipsum SIT Amit ...';
+  subject= 'Doller Ipsum SIT Amit-Sapna';
 }
